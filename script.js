c19b5055303b2031f823fb817fe2606a35f42358
Mantis_Prod <75092458+adridwk@users.noreply.github.com>
2021-06-20 22:17:03 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function palindrome(str) {
+//   const string = str.toLowerCase().replaceAll(/[\W_]+/g, ' ', '');
+//   const strArr = string.replaceAll(' ', '');
+//   let count = strArr.length - 1;
+//   let newStrState = true;
+//   for (const i of strArr) {
+//     i === strArr[count] ? '' : (newStrState = false);
+//     count--;
+//   }
+//   return newStrState;
+// }
+
+// console.log(palindrome('five|_/|four'));
+
+// picture: https://images.unsplash.com/photo-1539620944798-db3b7d352b6c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
+
+// lush https://images.unsplash.com/photo-1532796107-d570a19405ad?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80
+
+//lush perfumes https://images.unsplash.com/photo-1557827983-012eb6ea8dc1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80
